23fc3ae53e8cbc94319e3b8c0e11ee3679c41b04
tdzl2003 <tdzl2003@gmail.com>
2022-12-16 21:56:43 +0800

    fix: missing docs; add: random svg circle sample.

--- example/src/widgets/svg/index.tsx
+++ example/src/widgets/svg/index.tsx
@@ -1,16 +1,52 @@
-import { jsx } from 'mobxact';
+import { computed, IObservableArray, observable } from 'mobx';
+import { jsx, Element } from 'mobxact';
+
+interface Circle {
+  cx: number;
+  cy: number;
+  r: number;
+  stroke: string;
+  fill: string;
+}
 
 export function SvgTest() {
-  return (
-    <svg height="100" width="100">
+  const circles: IObservableArray<Element> = observable([]);
+
+  function randomColor() {
+    return (
+      '#' +
+      ('000000' + Math.floor(Math.random() * (1 << 24)).toString(16)).slice(-6)
+    );
+  }
+
+  function newCircle() {
+    circles.push(
       <circle
-        cx="50"
-        cy="50"
-        r="40"
-        stroke="black"
+        cx={Math.random() * 480}
+        cy={Math.random() * 640}
+        r={Math.random() * 100}
+        stroke={randomColor()}
+        fill={randomColor()}
         stroke-width="3"
-        fill="red"
       />
-    </svg>
+    );
+  }
+
+  function clear() {
+    circles.length = 0;
+  }
+
+  return (
+    <div>
+      <div>
+        <button onclick={newCircle}>Add Circle</button>
+        <button onclick={clear}>Clear</button>
+      </div>
+      <div>
+        <svg height="640" width="480">
+          {circles}
+        </svg>
+      </div>
+    </div>
   );
 }
